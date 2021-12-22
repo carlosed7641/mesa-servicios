056514ca341035833fb849b3e90250f2c920199b
Carlos Castro <ccastro46>
2021-12-22 12:32:44 -0500

    Página de agregar usuarios

---
+++ agregar-usuario.php
@@ -0,0 +1,80 @@
+<?php
+
+require 'includes/conexion.php';
+session_start();
+
+if ($_SESSION['tipo'] != 3) { 
+/** Si se intenta acceder a este sitio y no es admin,
+        o no está logueado, se redirigirá al index **/
+	header('location:index.php');
+}
+//Consulta para mostrar los tipos de usuarios
+$sql='SELECT * FROM tipo_usuario WHERE id_tipo_usuario BETWEEN 1 and 2';
+$resultado=$pdo->prepare($sql);
+$resultado->execute();
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Agregar Usuario</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/styles.css">
+   <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+   <link rel="stylesheet" href="assets/css/templatemo-breezed.css">
+   <link rel="stylesheet" href="assets/css/owl-carousel.css">
+   <link rel="stylesheet" href="assets/css/lightbox.css">
+</head>
+
+<body>
+
+<?php include('includes/header.php'); ?>  
+<a href="admin-usuarios.php" class="btn btn-primary mt-3 ml-3">Volver</a>
+<div class="container" style="margin-top: 0 auto; max-width: 38rem; ?>">
+		<div class="card border-success mb-3 mt-3 formulario" style="max-width: 55rem;">
+			<div class="card-header text-center" style="text-transform: uppercase;"><h4>Agregar Usuario</h4></div>
+			<form class="row" method="POST" action="admin-usuarios.php" style="margin: 0 5px;">
+				<div class="col-lg-6 col-md-6 text-center" style="margin-top: 65px;">
+					<img src="img/registro.png" style="max-width: 14rem;">
+				</div>
+				<div class="col-lg-6 col-md-6">
+						<br>
+						<!--Lista para los tipos de usuario-->
+						<select class="form-control" name="tipo_usuario" required="">
+							<option value="">--Seleccione una categoría--</option>
+							<?php
+							//Recorre todas los tipos y los imprime en una lista desplegable
+							while ($row=$resultado->FETCH(PDO::FETCH_ASSOC)) :?>
+						
+							<option value="<?php echo $row['id_tipo_usuario']; ?>"> <?php echo $row['tipo'];?></option>
+							
+							<?php endwhile; ?>
+						</select><br>			
+
+						<input name="nombre_completo" type="text" placeholder="Nombre completo" class="form-control" required=""> <br>
+						<input name="direccion" type="text" placeholder="Dirección" class="form-control" required=""> <br>
+						<input name="telefono" type="number" placeholder="Teléfono" class="form-control" required=""> <br>
+						<input name="usuario" type="text" placeholder="Usuario" class="form-control" required=""> <br>
+						<input name="password" type="text" placeholder="Contraseña" class="form-control" required=""> <br>
+
+			
+
+						<div class="text-center" style="margin-bottom: 2rem;">
+							<button class="btn btn-primary">Guardar</button>
+							<a href="agregar-usuario.php" class="btn btn-danger">Cancelar</a>
+						</div>
+
+				</div><br>
+			</form>
+		</div><br>
+</div>
+
+<?php include('includes/footer.php'); ?>  
